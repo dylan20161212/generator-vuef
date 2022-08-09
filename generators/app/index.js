692f1b036da954f013367b349011dfc012bac0e6
Your Name <you@example.com>
2022-08-09 11:41:31 +0800

    日志输出方式修改

--- generators/app/index.js
+++ generators/app/index.js
@@ -11,35 +11,38 @@ module.exports = class extends Generator {
         type: 'input',
         name: 'jdlName',
         message: '请输入您的jdl文件名',
-        default: 'E:/work/tongyimenhu/code/wisdomcity/jdls/jdlTest2.jdl'
+        default: 'E:/work/tongyimenhu/code/wisdomcity/jdls/jdlTest2.jdl',
+		store:true,
     },
 	{
         type: 'input',
         name: 'entityName',
         message: '请输入您的要生成的entity名称(默认值*代表所有，多个entity用英文,分隔）',
-        default: '*'
+        default: '*',
+		store:true,
     },
     {
         type: 'input',
         name: 'genPath',
         message: '请输入生成路径',
-        default: 'E:/work/tongyimenhu/code/test/gens'
+        default: 'E:/work/tongyimenhu/code/test/gens',
+		store:true,
     }
     ])
   }
  async writing(){
 	if(!this.answers.jdlName){
-		console.error('jdl 文件名不能为空');
+		this.log.error('jdl 文件名不能为空');
 		return;
 	}
 
 	if(!this.answers.entityName){
-		console.error('entity名称 不能为空');
+		this.log.error('entity名称 不能为空');
 		return;
 	}
 
 	if(!this.answers.genPath){
-		console.error('生成的路径不能为空');
+		this.log.error('生成的路径不能为空');
 		return;
 	}
 
@@ -48,17 +51,17 @@ module.exports = class extends Generator {
     try{
 		result = jhisterLib.parseFromFiles(files);
 	}catch(error){
-		console.error("jdl 解析异常，请核实路径或者文件内容");
+		this.log.error("jdl 解析异常，请核实路径或者文件内容");
 		return;
 	}
 
 	this.context = {};
     this.context["app"] = result;
-	//console.log(JSON.stringify(this.context));
-	//console.log("this.answers.entityName----------------"+this.answers.entityName);
+	//this.log(JSON.stringify(this.context));
+	//this.log("this.answers.entityName----------------"+this.answers.entityName);
 	if(result && this.answers.entityName !=='*'){
 		const entityNames = this.answers.entityName.split(',');
-		//console.log("execute----------------0");
+		//this.log("execute----------------0");
 		for(let entityName of entityNames){
 			this.context.entityName = entityName;
 			this._genViewFile();
@@ -69,7 +72,7 @@ module.exports = class extends Generator {
 
 
 	if(result && this.answers.entityName ==='*'){
-		//console.log("execute----------------1");
+		//this.log("execute----------------1");
 		const entityNames = result.entities.map(entity=>entity.name);
 		for(let entityName of entityNames){
 			this.context.entityName = entityName;
@@ -85,14 +88,14 @@ module.exports = class extends Generator {
 
    
    _genComponentFiles(){
-   		console.log("生成"+this.context.entityName+"的关系组件...");
+   		this.log("生成"+this.context.entityName+"的关系组件...");
 
 	   	const relationEntityNames = this.context.app.relationships.filter((item)=>{
-	   		console.log(item.from.name === this.context.entityName);
-	   		console.log(item.from.name +"==="+ this.context.entityName);
+	   		this.log(item.from.name === this.context.entityName);
+	   		this.log(item.from.name +"==="+ this.context.entityName);
 			return item.from.name === this.context.entityName
 		}).map(ele=>ele.to.name);
-	   	console.log("生成"+this.context.entityName+"的关系组件为："+relationEntityNames);
+	   	this.log("生成"+this.context.entityName+"的关系组件为："+relationEntityNames);
 	    const componentModels = this._getComponentModelFiles();
    		for(let entityName of relationEntityNames){
    			let componentContext = {};
@@ -104,16 +107,16 @@ module.exports = class extends Generator {
    			for(let m of componentModels){
    				//模板
    				// const tmpl = this.templatePath('vue_tls1/components'+m);
-   				// console.log("tmpl-->"+tmpl);
+   				// this.log("tmpl-->"+tmpl);
    				//const output = this.destinationPath(this.answers.genPath+'/components/'+fileName+"/"+fileName+'.vue')
-   				console.log("--------------------:"+this.templatePath('vue_tls1'));
+   				this.log("--------------------:"+this.templatePath('vue_tls1'));
    				const dest = this.answers.genPath+'/'+m.replaceAll("modelName",fileName).substring(this.templatePath('vue_tls1').length);
-   				console.log("output-->"+dest);
+   				this.log("output-->"+dest);
    				this.fs.copyTpl(m, dest, componentContext)
    			}
 
    		}
-   		console.log(this.context.entityName+"的关系组件生成完成");
+   		this.log(this.context.entityName+"的关系组件生成完成");
 
 
    }
@@ -121,22 +124,22 @@ module.exports = class extends Generator {
 
    _genViewFile(){
    	        this._genComponentFiles();
-   			console.log("生成"+this.context.entityName+" view 实体");
+   			this.log("生成"+this.context.entityName+" view 实体");
    			const entityName = this.context.entityName ;
 
 
-			//console.log(JSON.stringify(this.context));
+			//this.log(JSON.stringify(this.context));
 			const viewModels = this._getViewModelFiles();
 
 			for(let view of viewModels){
-				    console.log("开始生成模板..."+view)
+				    this.log("开始生成模板..."+view)
 					// 模板文件路径
 					//const tmpl = this.templatePath('vue_tls1/views'+view);
 					// 驼峰标示转为中划线
 					let fileName =entityName.replaceAll(/([a-z])([A-Z])/g,"$1-$2").toLowerCase();
 					//let fileName =entityName;
 					const dest = view.replaceAll("modelName",fileName).substring(this.templatePath('vue_tls1').length);
-					console.log("output-->"+dest);
+					this.log("output-->"+dest);
 					
 					// 输出文件路径
 					const output = this.destinationPath(this.answers.genPath+'/'+dest)
@@ -150,29 +153,29 @@ module.exports = class extends Generator {
 
 
    _getComponentModelFiles(){
-     	console.log("查看组件模板目录");
+     	this.log("查看组件模板目录");
      	 
 		const componentModels = [];
 		const tmpl = this.templatePath('vue_tls1/components');
 		this._readFileList(tmpl,componentModels);
-		console.log(componentModels);
+		this.log(componentModels);
 		return componentModels;
   }
 
 
    _getViewModelFiles(){
-     	console.log("查看视图模板目录");
+     	this.log("查看视图模板目录");
      	const componentModels = [];
      	const tmpl = this.templatePath('vue_tls1/views');
      	this._readFileList(tmpl,componentModels)
-     	console.log(componentModels);
+     	this.log(componentModels);
 		return componentModels;
   }
 
 
    _readFileList(dir, filesList = []) {
 	  const files = fs.readdirSync(dir);
-	  //console.log(files);
+	  //this.log(files);
 	  files.forEach((item, index) => {
 	    var fullPath = path.join(dir, item);
 	    const stat = fs.statSync(fullPath);
